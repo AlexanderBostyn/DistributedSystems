80e9a5e466fdaa6af2c35caa882d6eed7aa2e90f
vincent verbergt <vincent.verbergt@gmail.com>
2023-03-13 10:24:05 +0100

    Persistence test

--- Naming-server/src/test/java/com/groep5/Naming/server/PersistenceTest.java
+++ Naming-server/src/test/java/com/groep5/Naming/server/PersistenceTest.java
@@ -1,23 +1,28 @@
 package com.groep5.Naming.server;
 
 import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.api.Assertions;
+import java.io.File;
 import java.net.Inet4Address;
 import java.net.InetAddress;
-import java.net.InetSocketAddress;
 import java.util.HashMap;
 
 public class PersistenceTest {
-
+    private File file;
+    private HashMap<Integer, InetAddress> map = new HashMap<Integer, InetAddress>();
     @Test
-    public void testWrite() {
+    public void test() {
         try {
-            HashMap<Integer, InetAddress> map = new HashMap<Integer, InetAddress>();
             map.put(16, Inet4Address.getByName("127.0.0.1"));
-            Persistence.SaveMap(map);
-
+            this.file = Persistence.SaveMap(map, "data.json");
+            HashMap<Integer, InetAddress> loadedMap = Persistence.LoadMap(file);
+            System.out.println(map.get(16));
+            System.out.println(loadedMap.get(16));
+            Assertions.assertEquals(map, loadedMap);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
+
 }
