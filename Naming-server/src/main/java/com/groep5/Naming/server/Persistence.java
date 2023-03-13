8c900afcf3b0782bb8335883036120c55f391548
vincent verbergt <vincent.verbergt@gmail.com>
2023-03-13 09:47:05 +0100

    Save map method

--- Naming-server/src/main/java/com/groep5/Naming/server/Persistence.java
+++ Naming-server/src/main/java/com/groep5/Naming/server/Persistence.java
@@ -1,10 +1,43 @@
 package com.groep5.Naming.server;
 
-import java.net.Inet4Address;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.FileWriter;
+
+import java.net.InetAddress;
 import java.util.HashMap;
 
-public class SaveMap {
-    public static void (HashMap<int, Inet4Address> map) {
+public class Persistence {
 
+    /**
+     * Saves data to a file in the resources folder and returns that file
+     * @param map hashes of addresses we want to save
+     * @return saved file, null if error.
+     */
+    public static File SaveMap (HashMap<Integer, InetAddress> map, String fileName) {
+            try {
+                File file = new File("src/main/resources/" + fileName);
+                if (!file.exists()) {
+                    file.createNewFile();
+                }
+                FileWriter writer = new FileWriter("src/main/resources/" + fileName);
+                String mapJson = new ObjectMapper().writeValueAsString(map);
+                writer.write(mapJson);
+                writer.close();
+                return file;
+
+            } catch (Exception e) {
+                System.out.println("An error occurred");
+                e.printStackTrace();
+            }
+        return null;
     }
+
+    public static HashMap<Integer, InetAddress> LoadMap(File file) {
+        
+    }
+
+
+
 }
