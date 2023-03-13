2ceae92c66eaeac7d2f62f4a0378df9c61df7dcd
vincent verbergt <vincent.verbergt@gmail.com>
2023-03-13 09:47:19 +0100

    test save map method

--- Naming-server/src/test/java/com/groep5/Naming/server/PersistenceTest.java
+++ Naming-server/src/test/java/com/groep5/Naming/server/PersistenceTest.java
@@ -1,2 +1,23 @@
-package com.groep5.Naming.server;public class PersistenceTest {
+package com.groep5.Naming.server;
+
+import org.junit.jupiter.api.Test;
+
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.util.HashMap;
+
+public class PersistenceTest {
+
+    @Test
+    public void testWrite() {
+        try {
+            HashMap<Integer, InetAddress> map = new HashMap<Integer, InetAddress>();
+            map.put(16, Inet4Address.getByName("127.0.0.1"));
+            Persistence.SaveMap(map);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
