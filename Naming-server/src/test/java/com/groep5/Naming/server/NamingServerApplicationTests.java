ed99d68afed689748c6727706de96563d2e7a683
Hendrik Valcke <hendrik.valcke@hotmail.be>
2023-03-20 10:12:07 +0100

    fixed testbug

--- Naming-server/src/test/java/com/groep5/Naming/server/NamingServerApplicationTests.java
+++ Naming-server/src/test/java/com/groep5/Naming/server/NamingServerApplicationTests.java
@@ -3,7 +3,7 @@ package com.groep5.Naming.server;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest (classes = NamingServerApplicationTests.class)
 class NamingServerApplicationTests {
 
 	@Test
