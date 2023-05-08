454d6cc81fc2bbb4277194d6337e7a635b9c9e95
vincent verbergt <vincent.verbergt@gmail.com>
2023-05-08 10:00:36 +0200

    fsd

--- deployNamingServer.sh
+++ deployNamingServer.sh
@@ -5,7 +5,7 @@ git pull origin test
 mvn clean install
 rm src/main/resources/data.json
 touch src/main/resources/data.json
-echo '{}' > filename.txt
+echo '{}' > src/main/resources/data.json
 java -jar target/Naming-server-0.0.1-SNAPSHOT.jar
 #Your awesome titleclear data.json
 #mvn clean install
