4351b36356859694523761069a23cba7bf77b479
vincent verbergt <vincent.verbergt@gmail.com>
2023-05-12 15:53:10 +0200

    improved script

--- deployNode.sh
+++ deployNode.sh
@@ -6,7 +6,10 @@ cd Node/
 git reset --hard origin/test
 git pull origin test
 chmod +x ../deployNode.sh
+rm -r src/main/resources/local/*
+rm -r src/main/resources/replicated/*
 mkdir src/main/resources/local
+mkdir src/main/resources/replicated
 touch src/main/resources/local/$1.file
 mvn clean install
 java -jar target/Node-0.0.1-SNAPSHOT.jar $1
