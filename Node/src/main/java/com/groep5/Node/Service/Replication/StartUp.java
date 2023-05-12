1bd0641edb40478de71c38c977e9405eaae38a7f
vincent verbergt <vincent.verbergt@gmail.com>
2023-05-12 14:04:08 +0200

    Fixed null pointer error

--- Node/src/main/java/com/groep5/Node/Service/Replication/StartUp.java
+++ Node/src/main/java/com/groep5/Node/Service/Replication/StartUp.java
@@ -4,18 +4,23 @@ import com.groep5.Node.Node;
 import com.groep5.Node.SpringContext;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Logger;
 
 public class StartUp {
     public Node node;
-    public File[] files;
-    private Logger logger = Logger.getLogger(this.getClass().getName());
+    public ArrayList<File> files = new ArrayList<>();
+    private final Logger logger = Logger.getLogger(this.getClass().getName());
 
     public void lookForFiles() {
         File directory = new File("src/main/resources/local");
-        files = directory.listFiles();
-        logger.info("Files: " + Arrays.toString(files));
+        File[] fileArray = directory.listFiles();
+        if (fileArray != null) {
+            files.addAll(List.of(fileArray));
+        }
+        logger.info("Files: " + Arrays.toString(files.toArray()));
     }
     public void sendFiles() {
         for(File file : files) {
