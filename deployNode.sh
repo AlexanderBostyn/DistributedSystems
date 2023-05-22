58668330a1db23cbc85bcfc69f6bd8761fdd6c04
vincent verbergt <vincent.verbergt@gmail.com>
2023-05-22 09:39:47 +0200

    updated deployscritp

--- deployNode.sh
+++ deployNode.sh
@@ -10,7 +10,7 @@ rm -r src/main/resources/local/*
 rm -r src/main/resources/replicated/*
 mkdir src/main/resources/local
 mkdir src/main/resources/replicated
-touch src/main/resources/local/$1.file
 mvn clean install
+touch src/main/resources/local/$1.file
 java -jar target/Node-0.0.1-SNAPSHOT.jar $1
 
