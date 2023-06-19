d963d3836c50fd3b6bf3f1b8df349ee9fa770195
Hendrik Valcke <hendrik.valcke@student.uantwerpen.be>
2023-06-19 09:33:19 +0200

    toggle print requests through api call

--- Naming-server/src/main/java/com/groep5/Naming/server/Controller/HashController.java
+++ Naming-server/src/main/java/com/groep5/Naming/server/Controller/HashController.java
@@ -6,6 +6,7 @@ import com.groep5.Naming.server.Service.Hasher;
 import org.apache.juli.logging.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,6 +19,7 @@ import java.util.logging.Logger;
 public class HashController {
     private Logger logger = Logger.getLogger(this.getClass().getName());
     private Hasher hasher;
+    private boolean printRequests=false;
     HashController(ApplicationContext context) {
         this.hasher = new SHAHasher(context);
     }
@@ -26,24 +28,37 @@ public class HashController {
     @RequestMapping({"/","/home"})
     @ResponseBody
     public String showHomepage(){
-        logger.info("incoming request /home");
+        if (printRequests){
+            logger.info("incoming request /home");}
         return "Hello World, naming server here";
+    }
+
+    @PutMapping("/setPrintRequests")
+    public ResponseEntity<String> setPrintRequests(@RequestBody boolean bool){
+        printRequests=bool;
+        return new ResponseEntity<String>("printRequests set to"+bool, HttpStatus.OK);
     }
 
     @GetMapping("/file/{id}")//locate the node a file is located at
     public String locateFileById(@PathVariable int id) throws UnknownHostException {
-        logger.info("incoming GET /file/"+id);
+        if (printRequests){
+            logger.info("incoming GET /file/"+id);
+        }
         return hasher.locateFileById(id).getHostAddress();
     }
     @GetMapping("/file")//locate the node a file is located at
     public String  locateFileByName(@RequestBody String name) throws UnknownHostException {
-        logger.info("incoming GET /file");
+        if (printRequests){
+            logger.info("incoming GET /file");
+        }
         return hasher.locateFileByName(name).getHostAddress();
     }
 
     @DeleteMapping("/file")
     public String locateFileByNameAndRemoveNode(@RequestBody String name) throws UnknownHostException {
-        logger.info("incoming DELETE /file");
+        if (printRequests){
+            logger.info("incoming DELETE /file");
+        }
         InetAddress nodeAddress = hasher.locateFileByName(name);
         hasher.deleteNodeByAddress(nodeAddress);
         return nodeAddress.toString();
@@ -51,7 +66,9 @@ public class HashController {
 
     @GetMapping("/node/{id}")
     public ResponseEntity<String> locateNodeById(@PathVariable int id) {
-        logger.info("incoming GET /node/"+id);
+        if (printRequests){
+            logger.info("incoming GET /node/"+id);
+        }
         InetAddress address = hasher.locateNodeById(id);
         if (address == null) {
             return new ResponseEntity<String>("Node Not Found", HttpStatusCode.valueOf(404));
@@ -61,26 +78,34 @@ public class HashController {
 
     @GetMapping("/node/{id}/previous")
     public int getPreviousNodeId(@PathVariable int id) {
-        logger.info("incoming GET /node/"+id+"/previous");
+        if (printRequests){
+            logger.info("incoming GET /node/"+id+"/previous");
+        }
         return hasher.previousId(id);
     }
 
     @GetMapping("node/{id}/next")
     public int getNextNodeId(@PathVariable int id) {
-        logger.info("incoming GET /node/"+id+"/next");
+        if (printRequests){
+            logger.info("incoming GET /node/"+id+"/next");
+        }
         return hasher.nextId(id);
     }
 
     @PostMapping("/node/{name}")//add a node (with address) and receive hash id
     public int addNode(@PathVariable String name, @RequestBody String strAddress) throws UnknownHostException {
-        logger.info("incoming POST /node/"+name);
+        if (printRequests){
+            logger.info("incoming POST /node/"+name);
+        }
         logger.info("Added Node:" + name);
         return hasher.addNode(name,strAddress);
     }
 
     @DeleteMapping("/node/{id}")//delete a node
     public void deleteNodeByAddress(@PathVariable int id) throws UnknownHostException {
-        logger.info("incoming DELETE /node/"+id);
+        if (printRequests){
+            logger.info("incoming DELETE /node/"+id);
+        }
         logger.info("deleted Node with Id: " + id );
         hasher.deleteNode(id);
     }
@@ -88,14 +113,18 @@ public class HashController {
     @GetMapping("/hash/{name}")
     public int calcHashValue(@PathVariable String name)
     {
-        logger.info("incoming GET /node/"+name);
+        if (printRequests){
+            logger.info("incoming GET /node/"+name);
+        }
         return hasher.calcHashId(name);
     }
 
 
     @GetMapping("/discovery")
     public int returnAmountOfNodes(@RequestBody String name, @RequestBody String strAddress) throws UnknownHostException {
-        logger.info("incoming GET /discovery");
+        if (printRequests){
+            logger.info("incoming GET /discovery");
+        }
         hasher.addNode(name, strAddress);
         int size = hasher.returnAmountOfNodes();
         //multicast networksize to other nodes
