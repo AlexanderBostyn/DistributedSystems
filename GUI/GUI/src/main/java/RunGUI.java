c5fd6a5b5a9387902096dbc771ce32fed3fc6f5b
Alexander Bostyn <alexander.bostyn@gmail.com>
2023-06-19 12:37:33 +0200

    HTTP

--- GUI/GUI/src/main/java/RunGUI.java
+++ GUI/GUI/src/main/java/RunGUI.java
@@ -1,11 +1,24 @@
 import MainPanel.MainFrame;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import javax.swing.*;
 import java.io.IOException;
+import java.net.ServerSocket;
 
 public class RunGUI {
     public static void main(String args[]) throws IOException  //static method
     {
+        System.out.println("test");
+        ServerSocket socket = new ServerSocket(54321);
+        String address = String.valueOf(socket.getInetAddress());
+        System.out.println(address);
+        String test =  WebClient.create("http://" + address + ":54321")
+                .get()
+                .uri("/status")
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+        System.out.println(test);
         MainFrame mf = new MainFrame();
         mf.setExtendedState(JFrame.MAXIMIZED_BOTH);
     }
