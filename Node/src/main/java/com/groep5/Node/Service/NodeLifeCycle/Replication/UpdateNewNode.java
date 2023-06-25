d406acd97fbea3b62f392a5ff521db5c4a10875a
Hendrik Valcke <hendrik.valcke@student.uantwerpen.be>
2023-06-25 23:29:04 +0200

    fix check for local files in log

--- Node/src/main/java/com/groep5/Node/Service/NodeLifeCycle/Replication/UpdateNewNode.java
+++ Node/src/main/java/com/groep5/Node/Service/NodeLifeCycle/Replication/UpdateNewNode.java
@@ -134,19 +134,20 @@ public class UpdateNewNode {
         }
         if (sentLog.size() > 0) {
             for (Log.LogEntry entry :sentLog.getEntrySet()) {
-                if (ReplicationService.listDirectory("src/main/resources/local").stream().noneMatch(localFile -> localFile.getName().equals(entry.getFileName())))
-                {
-                    logger.info("log contains local file: "+entry.getFileName());
-                    for (Inet4Address address: entry.getAddresses()) {
-                        if (address!=nodePropreties.getNodeAddress() && address!=ip)
-                        {
-                            logger.info("delete non local, non owner address: "+address);
-                            sentLog.delete(entry.getFileName(),address);
+                for (File localFile:localFiles) {
+                    if (localFile.getName().equals(entry.getFileName())){
+                        logger.info("log contains local file: "+entry.getFileName());
+                        for (Inet4Address address: entry.getAddresses()) {
+                            if (address!=nodePropreties.getNodeAddress() && address!=ip)
+                            {
+                                logger.info("delete non local, non owner address: "+address);
+                                sentLog.delete(entry.getFileName(),address);
+                            }
                         }
-
                     }
                 }
             }
+            logger.info("send log");
             UnicastSender.sendLog(sentLog, ip);
         }
     }
