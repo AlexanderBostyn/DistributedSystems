28671c373b3eb1c223a251ece27532e35edbc072
Hendrik Valcke <hendrik.valcke@student.uantwerpen.be>
2023-06-25 22:54:05 +0200

    loggers for newNectNodeHash and fileHash when at start of ring

--- Node/src/main/java/com/groep5/Node/Service/NodeLifeCycle/Replication/UpdateNewNode.java
+++ Node/src/main/java/com/groep5/Node/Service/NodeLifeCycle/Replication/UpdateNewNode.java
@@ -89,10 +89,12 @@ public class UpdateNewNode {
             // This creates problems for linearity that can be fixed if we just add 32768 to all the hashes that are at the beginning of the ring
             if (newNextNodeHash < nodePropreties.nodeHash) {
                 newNextNodeHash += 32768;
+                logger.info("new next node at start of ring, add 32768: "+ newNextNodeHash);
 
                 // The file is also located at the beginning of the ring, adding 32768 will make the ring linear again.
                 if (fileHash < nodePropreties.nodeHash) {
                     fileHash += 32768;
+                    logger.info("file id is at start of ring, add 32768: "+ fileHash);
                 }
             }
             if (fileHash > newNextNodeHash) {
